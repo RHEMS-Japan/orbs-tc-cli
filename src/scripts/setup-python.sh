5580cc3a354b928c58366434ec7834a02922a77e
shindex-rhems <shindex@rhems-japan.co.jp>
2022-01-18 15:41:53 +0900

    test

---
+++ src/scripts/setup-python.sh
@@ -0,0 +1,13 @@
+which apt-get 2> /dev/null
+
+if [ $? -eq 0 ]; then
+    export CMD=apt-get
+else
+    export CMD=yum
+fi
+
+sudo ${CMD} -y update
+sudo ${CMD} -y upgrade
+sudo ${CMD} -y install python3-pip
+echo "python version: $(python3 --version)"
+echo "pip version: $(pip3 --version)"
