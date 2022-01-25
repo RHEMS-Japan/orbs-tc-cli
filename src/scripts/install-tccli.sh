609c68db4feb3db7fb49bd3644e824d655227939
shindex-rhems <shindex@rhems-japan.co.jp>
2022-01-25 14:09:24 +0900

    pip

--- src/scripts/install-tccli.sh
+++ src/scripts/install-tccli.sh
@@ -18,9 +18,12 @@ else
 
     echo "=== python version ==="
     python3 --version
+
+    curl https://bootstrap.pypa.io/get-pip.py --output get-pip.py
+    python3 get-pip.py
 fi
 
-python3 -m pip install tccli-intl-en
+pip install tccli-intl-en
 
 echo "=== tccli version ==="
 tccli --version
