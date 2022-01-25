ee5bb52532671e5475bbdfb464e3f971f081bc58
shindex-rhems <shindex@rhems-japan.co.jp>
2022-01-25 13:06:46 +0900

    fix install

--- src/scripts/install-tccli.sh
+++ src/scripts/install-tccli.sh
@@ -3,7 +3,7 @@ if which tccli; then
     exit 0
 fi
 
-if [ "$(which python3)" ] && [ "$(which pip3)" ]; then
+if [ "$(which python3)" ]; then
     echo "already set up."
 else
     if which apt-get; then
@@ -18,12 +18,9 @@ else
 
     echo "=== python version ==="
     python3 --version
-
-    echo "=== pip version ==="
-    pip3 --version
 fi
 
-pip3 install tccli-intl-en
+pip install tccli-intl-en
 
 echo "=== tccli version ==="
 tccli --version
